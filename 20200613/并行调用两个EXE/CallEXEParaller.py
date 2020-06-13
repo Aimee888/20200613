947fb9908cd0feb6677df5256a98abd495fef696
Aimee <961745931@qq.com>
2020-06-13 15:18:00 +0800

    20200613

---
+++ 20200613/并行调用两个EXE/CallEXEParaller.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# _*_ coding: UTF-8 _*_
+"""=================================================
+@Project -> File    : 20200613 -> CallEXEParaller.py
+@IDE     : PyCharm
+@Author  : Aimee
+@Date    : 2020/6/13 14:20
+@Desc    :目的：可以并行调用两个或者多个EXE
+================================================="""
+import subprocess
+import sys
+import os
+import threading
+
+
+SECONDS_TIMEOUT = 1000
+
+
+def execute_exe(exe_path, exe_param):
+    current_path = os.getcwd()
+    folder_path, file_name = os.path.split(exe_path)
+    os.chdir(folder_path)
+    p = subprocess.Popen(exe_path + " " + exe_param, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    try:
+        p.wait(timeout=SECONDS_TIMEOUT)
+    except Exception as e:
+        p.kill()
+        os.chdir(current_path)
+        print("===== process timeout ======")
+        return None
+    except:
+        s = sys.exc_info()
+        str_error = "Error '%s' happened on line %d" % (s[1], s[2].tb_lineno)
+        p.kill()
+        os.chdir(current_path)
+        return None
+    os.chdir(current_path)
+    output = p.communicate()[0]
+    err = p.communicate()[1]
+    print(output)
+    print(p.returncode)
+
+
+if __name__ == '__main__':
+    t1 = threading.Thread(target=execute_exe, args=("rose.exe", ""))
+    t2 = threading.Thread(target=execute_exe, args=("CPUUsage.exe", ""))
+    t1.start()
+    t2.start()
---
+++ 20200613/并行调用两个EXE/README.md
@@ -0,0 +1,14 @@
+# CallEXEParaller.py
+### 目的
+并行调用两个或两个以上的EXE程序  
+### 导入库
+subprocess，threading  
+### 方法
+开启多线程运行调用exe的函数  
+### 功能  
+rose.exe 画一朵玫瑰花  
+CPUUsage.exe 画一个CPU使用率的折线图
+### Rose动画
+![Rose](./img_folder/rose.gif)
+### Usage动态折线图
+![Usage](./img_folder/usage.gif)
